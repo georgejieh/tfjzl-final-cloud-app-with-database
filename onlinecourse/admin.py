cf0dfa414c55bc276448afab711b4f600bee33c0
george.jieh <george.jieh@gmail.com>
2025-01-16 18:14:16 -0500

    create QuesntionInline and ChoiceInline classes.

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
 
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
 
 class LessonInline(admin.StackedInline):
     model = Lesson
